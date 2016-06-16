948337dc6d235078c166e5f8b40d968e20b3060f
Nkt0110 <nkt011094@gmail.com>
2016-06-16 14:00:43 +0300

    Add multidex library. Extend App from MultiDexApplication

--- app/src/main/java/digitalpromo/cabsdemo/App.java
+++ app/src/main/java/digitalpromo/cabsdemo/App.java
@@ -5,13 +5,21 @@ import android.app.Application;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 import android.view.WindowManager;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
 
-public class App extends Application implements Application.ActivityLifecycleCallbacks {
+public class App extends MultiDexApplication implements Application.ActivityLifecycleCallbacks {
     public static final String TAG = App.class.getSimpleName();
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 
     private static Context context;
 
---
+++ app/src/main/java/digitalpromo/cabsdemo/api/new_api/GetOrderHistoryResponse.java
@@ -0,0 +1,7 @@
+package digitalpromo.cabsdemo.api.new_api;
+
+/**
+ * Created by Администратор on 16.06.2016.
+ */
+public class GetOrderHistoryResponse {
+}

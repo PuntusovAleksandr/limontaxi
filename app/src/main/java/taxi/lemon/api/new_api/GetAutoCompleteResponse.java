c71af18534c0e8f28567432140a5e7df090618dc
Nkt0110 <nkt011094@gmail.com>
2016-07-01 09:26:06 +0300

    Start change autocomplete request and response

--- app/src/main/java/taxi/lemon/api/new_api/GetAutoCompleteResponse.java
+++ app/src/main/java/taxi/lemon/api/new_api/GetAutoCompleteResponse.java
@@ -10,10 +10,25 @@ import taxi.lemon.models.RouteItem;
  * Created by Администратор on 14.06.2016.
  */
 public class GetAutoCompleteResponse {
-    @SerializedName("geo_street")
-    private ArrayList<RouteItem> autocomplete;
+    @SerializedName("geo_streets")
+    private ArrayList<GeoStreet> geoStreets;
+
+    @SerializedName("geo_objects")
+    private ArrayList<GeoObject> geoObjects;
+
+    private ArrayList<RouteItem> items = new ArrayList<>();
 
     public ArrayList<RouteItem> getAutocomplete() {
-        return autocomplete;
+        return null;
+    }
+
+    protected class GeoStreet {
+        @SerializedName("geo_street")
+        protected ArrayList<RouteItem> autocomplete1;
+    }
+
+    protected class GeoObject {
+        @SerializedName("geo_object")
+        protected ArrayList<RouteItem> autocomplete2;
     }
 }

d67f7f8da80d08a212d043eec944ffd05ca5fb25
Nkt0110 <nkt011094@gmail.com>
2016-07-01 15:56:30 +0300

    Start creating OrderInfoActivity for push messages

---
+++ app/src/main/java/taxi/lemon/activities/OrderInfoActivity.java
@@ -0,0 +1,15 @@
+package taxi.lemon.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import taxi.lemon.R;
+
+public class OrderInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_info);
+    }
+}
